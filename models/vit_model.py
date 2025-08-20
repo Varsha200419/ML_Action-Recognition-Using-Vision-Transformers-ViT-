ecf84d9e2af6676ef8906d33f90e2967a5a078d5
ILAKKIYAVARSHA KANNAPPAN <ilakkiyavarshakannan19@gmail.com>
2025-08-20 23:14:06 +0100

    Delete models/vit_model.py

--- models/vit_model.py
+++
@@ -1,8 +0,0 @@
-# Model utilities for ViT
-# Add utility functions for ViT here
-
-from .timesformer_model import get_timesformer_model
-from .vit_base_model import get_vit_model
-# Remove or comment out the following if videomae_model.py does not exist
-# from .videomae_model import get_videomae_model
-    return VideoViTModel(num_classes)
